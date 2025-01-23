16b500355e8c3a426735ca4ac330c4555cf9927a
Ewillian9 <janonguittard@gmail.com>
2025-01-23 15:43:56 +0100

    J'ai oublié de faire les comits oupsi

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Event;
+use App\Entity\Speaker;
+use App\Entity\Post;
+use App\Repository\EventRepository;
+use App\Repository\PostRepository;
+use App\Repository\SpeakerRepository;
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+
+class DashboardController extends AbstractDashboardController
+{
+    // Declaratiton of repositories
+    private EventRepository $eventRepository;
+    private SpeakerRepository $speakerRepository;
+    private PostRepository $postRepository;
+
+    // Constructor
+    public function __construct(
+        EventRepository $eventRepository,
+        SpeakerRepository $speakerRepository,
+        PostRepository $postRepository
+    ) {
+        $this->eventRepository = $eventRepository;
+        $this->speakerRepository = $speakerRepository;
+        $this->postRepository = $postRepository;
+
+    }
+
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        $events = $this->eventRepository->findAll(); // All events
+        $speakers = $this->speakerRepository->findAll(); // All speakers
+        $posts = $this->postRepository->findAll(); // All posts
+        $pastEvents = [];
+        foreach ($events as $evt) {
+            if ($evt->getDate() < new \DateTime()) {
+                array_push($pastEvents, $evt);
+            }
+        } // Past events
+        return $this->render('admin/dashboard.html.twig', [
+            'events' => $events,
+            'speakers' => $speakers,
+            'pastEvents' => $pastEvents,
+            'posts' => $posts
+        ]);
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Supatalks');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('Events', 'fas fa-list', Event::class);
+        yield MenuItem::linkToCrud('Speakers', 'fas fa-bullhorn', Speaker::class);
+        yield MenuItem::linkToCrud('Posts', 'fas fa-table-list', Post::class);
+        yield MenuItem::linkToRoute('Back to website', 'fa fa-arrow-left', 'app_home');
+
+        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+    }
+} // Do not write anything after this line
---
+++ src/Controller/Admin/PostCrudController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Post;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
+
+class PostCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Post::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setPageTitle('index', '🧾 All posts')
+            ->setEntityLabelInSingular('Post')
+            ->setEntityLabelInPlural('Posts')
+            ->setSearchFields(['title'])
+            ->setDefaultSort(['title' => 'ASC']);
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->add(Crud::PAGE_INDEX, Action::DETAIL);
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            FormField::addPanel('Post Information'),
+            TextField::new('title')
+                ->setLabel('Title')
+                ->setHelp('Set the title of the post'),
+            TextField::new('content')
+                ->setLabel('Content')
+                ->setHelp('Set the content of the post')
+                    ->hideOnIndex(),
+            BooleanField::new('isPublished')
+                ->setLabel('Published?')
+                ->setHelp('Is the post published?'),
+            ImageField::new('image')
+                ->setLabel('📷 Image')
+                ->setHelp('Choose an image for the post')
+                ->setUploadedFileNamePattern('[randomhash].[extension]')
+                ->setUploadDir('public/uploads/speakers')
+                ->setBasePath('uploads/speakers'),
+        ];
+    }
+}
